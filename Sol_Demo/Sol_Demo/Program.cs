82a526124fb5745dc1e48a0331b147b5c3e4ba77
Kishor Naik <kishor.naik011.net@outlook.com>
2020-09-28 17:47:42 +0530

    Code Commit

---
+++ Sol_Demo/Sol_Demo/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace Sol_Demo
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            Singleton singletonDemo1 = Singleton.Insatnce;
+            singletonDemo1.Access();
+
+            Singleton singletonDemo2 = Singleton.Insatnce;
+            singletonDemo2.Access();
+
+            //Using Parallel
+            Parallel.Invoke(
+                () =>
+                {
+                    Singleton singletonDemo3 = Singleton.Insatnce;
+                    singletonDemo3.Access();
+                }, // Thread 1
+                () =>
+                {
+                    Singleton singletonDemo4 = Singleton.Insatnce;
+                    singletonDemo4.Access();
+                } // Thread 2
+                );
+        }
+    }
+
+    public class Singleton
+    {
+        private static readonly Lazy<Singleton> singleton = new Lazy<Singleton>(() => new Singleton()); // Thread Safe.
+
+        //// for demo purpose, How many instance will create.
+        //private static int counter;
+
+        private Singleton()
+        {
+            //counter++;
+            //Console.WriteLine($"Counter Instance : {counter}");
+        }
+
+        public static Singleton Insatnce => singleton.Value;
+
+        public void Access()
+        {
+            Console.WriteLine("Singleton demo");
+        }
+    }
+}
